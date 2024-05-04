cc1b8465044c2c11b78dbc24becb71c7b96b4fe1
Eduardo Villamil <eduardo.villamil37830@ucaldas.edu.co>
2024-05-04 10:50:02 -0500

    instalación y configuración de Flowbite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EncabezadoComponent } from "./publico/pagina-maestra/encabezado/encabezado.component";
+import { PiePaginaComponent } from "./publico/pagina-maestra/pie-pagina/pie-pagina.component";
+import { MenuLateralComponent } from "./publico/pagina-maestra/menu-lateral/menu-lateral.component";
+import { initFlowbite } from 'flowbite';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, EncabezadoComponent, PiePaginaComponent, MenuLateralComponent]
 })
-export class AppComponent {
-  title = 'funeraria-digital';
+export class AppComponent implements OnInit {
+  title = 'web-app';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
