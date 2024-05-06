dae9c199d58eba93517a873fa3c3fdd5171814a5
Eduardo Villamil <eduardo.villamil37830@ucaldas.edu.co>
2024-05-05 22:06:16 -0500

    Avances según el video 36, aunque falta un poco

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './publico/inicio/inicio.component';
+import { RutaNoEncontradaComponent } from './publico/errores/ruta-no-encontrada/ruta-no-encontrada.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'inicio',
+        component: InicioComponent
+    },
+    {
+        path: '',
+        redirectTo: '/inicio',
+        pathMatch: 'full'
+    },
+    {
+        path: 'seguridad',
+        loadChildren: () => import('./modulos/seguridad/seguridad.module').then(m => m.SeguridadModule)
+    },
+    {
+        path: '**',
+        component: RutaNoEncontradaComponent
+    }
+];
--- src/app/modulos/seguridad/seguridad-routing.module.ts
+++ src/app/modulos/seguridad/seguridad-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IdentificarUsuarioComponent } from './identificar-usuario/identificar-usuario.component';
+import { CambioClaveComponent } from './cambio-clave/cambio-clave.component';
+import { RecuperarClaveComponent } from './recuperar-clave/recuperar-clave.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'identificar-usuario',
+    component: IdentificarUsuarioComponent
+  },
+  {
+    path: 'cambiar-clave',
+    component: CambioClaveComponent
+  },
+  {
+    path: 'recuperar-clave',
+    component: RecuperarClaveComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
