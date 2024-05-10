efca98d5db67bd0961165727e40288b8f33db7e2
Eduardo Villamil <eduardo.villamil37830@ucaldas.edu.co>
2024-05-09 22:00:52 -0500

    desarrollo de formulario de identificación con validación de campos

--- src/app/modulos/seguridad/identificacion-usuario/identificacion-usuario.component.ts
+++ src/app/modulos/seguridad/identificacion-usuario/identificacion-usuario.component.ts
@@ -1,12 +1,49 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-identificacion-usuario',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule
+  ],
   templateUrl: './identificacion-usuario.component.html',
   styleUrl: './identificacion-usuario.component.css'
 })
 export class IdentificacionUsuarioComponent {
+  fGroup: FormGroup = new FormGroup({});
+  constructor(
+    private fb: FormBuilder
+  ) { 
+    
+  }
 
+  ngOnInit() {
+    this.ContruirFormulario();
+  }
+
+  ContruirFormulario() {
+    this.fGroup = this.fb.group({
+      usuario: ['', [Validators.required, Validators.email]],
+      clave: ['', [Validators.required]]
+    });
+  }
+
+  IdentificarUsuario() {
+    if (this.fGroup.invalid) {
+      alert('Debe ingresar los campos requeridos');
+      return;
+    } else {
+      alert('Ingresando al sistema');
+    }
+  }
+
+  get obtenerFormGroup() {
+    return this.fGroup.controls;
+  }
 }
