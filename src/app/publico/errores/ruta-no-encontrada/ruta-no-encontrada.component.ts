ec30fc30cde6b1b62a28b26b1f42670d3cb8ea19
Eduardo Villamil <eduardo.villamil37830@ucaldas.edu.co>
2024-05-21 09:15:09 -0500

    Componentes de Contacto y Nosotros, y ajuste pequeño en menú lateral

--- src/app/publico/errores/ruta-no-encontrada/ruta-no-encontrada.component.ts
+++ src/app/publico/errores/ruta-no-encontrada/ruta-no-encontrada.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-ruta-no-encontrada',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './ruta-no-encontrada.component.html',
   styleUrl: './ruta-no-encontrada.component.css'
 })
