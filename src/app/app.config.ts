5938bb50cbfd9f58d966c89a763df5a5a75a8f72
Eduardo Villamil <eduardo.villamil37830@ucaldas.edu.co>
2024-05-27 13:03:52 -0500

    se agregó permisos.module, interceptor y algunos cambios en el darkmode

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,11 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withFetch } from '@angular/common/http';
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 import { BarController, Colors, Legend } from 'chart.js';
 import { MAT_DATE_FORMATS } from '@angular/material/core';
+import { AuthInterceptor } from './interceptors/auth.interceptors';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -15,6 +16,7 @@ export const appConfig: ApplicationConfig = {
     ), provideCharts(withDefaultRegisterables()),
     provideCharts(withDefaultRegisterables()),
     provideCharts({ registerables: [BarController, Legend, Colors] }),
-    { provide: MAT_DATE_FORMATS, useValue: { display: { dateInput: 'YYYY/DD/MM' } } }
+    { provide: MAT_DATE_FORMATS, useValue: { display: { dateInput: 'YYYY/DD/MM' } } },
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
   ]
 };
